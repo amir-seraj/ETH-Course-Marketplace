a0705bc93e02853f419d561359f47f7ae408e607
amirseraj <amirseraj.ir@gmail.com>
2022-10-12 16:37:44 +0330

    getter functions and course storing wired up

--- contracts/CourseMarketplace.sol
+++ contracts/CourseMarketplace.sol
@@ -1,13 +1,11 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
-
 contract CourseMarketplace {
-
   enum State {
     Purchased,
     Activated,
     Deactivated
   }
-
   struct Course {
     uint id; // 32
     uint price; // 32
@@ -15,20 +13,52 @@ contract CourseMarketplace {
     address owner; // 20
     State state; // 1
   }
+  // mapping of courseHash to Course data
+  mapping(bytes32 => Course) private ownedCourses;
+  // mapping of courseID to courseHash
+  mapping(uint => bytes32) private ownedCourseHash;
+  // number of all courses + id of the course
+  uint private totalOwnedCourses;
   function purchaseCourse(
-    bytes16 courseId,
-    bytes32 proof
-  ) external 
-    payable 
-    returns(bytes32) 
-    {
-    // course id - 10
-    // 0x00000000000000000000000000003130
-    // 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
-    // 000000000000000000000000000031305B38Da6a701c568545dCfcB03FcB875f56beddC4
-    // keccak256 - c4eaa3558504e2baa2669001b43f359b8418b44a4477ff417b4b007d7cc86e37
-    // - c4eaa3558504e2baa2669001b43f359b8418b44a4477ff417b4b007d7cc86e37
-    bytes32 courseHash=keccak256(abi.encodePacked(courseId, msg.sender));
-    return courseHash;
+    bytes16 courseId, // 0x00000000000000000000000000003130
+    bytes32 proof // 0x0000000000000000000000000000313000000000000000000000000000003130
+  )
+    external
+    payable
+  {
+    bytes32 courseHash = keccak256(abi.encodePacked(courseId, msg.sender));
+    uint id = totalOwnedCourses++;
+    ownedCourseHash[id] = courseHash;
+    ownedCourses[courseHash] = Course({
+      id: id,
+      price: msg.value,
+      proof: proof,
+      owner: msg.sender,
+      state: State.Purchased
+    });
+  }
+
+  function getCourseCount()
+    external
+    view
+    returns (uint)
+  {
+    return totalOwnedCourses;
+  }
+
+  function getCourseHashAtIndex(uint index)
+    external
+    view
+    returns (bytes32)
+  {
+    return ownedCourseHash[index];
+  }
+
+  function getCourseByHash(bytes32 courseHash)
+    external
+    view
+    returns (Course memory)
+  {
+    return ownedCourses[courseHash];
   }
 }
