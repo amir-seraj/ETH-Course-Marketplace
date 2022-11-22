532b7c5d92c30bcb38d856ddcc385d2c1d0eecb3
mummy <amirseraj.ir@gmail.com>
2022-11-22 20:46:55 +0330

    handle wallet changes

--- components/hooks/web3/index.js
+++ components/hooks/web3/index.js
@@ -1,94 +1,103 @@
+import { useHooks } from "@components/providers/web3";
+import { useEffect } from "react";
+import { useWeb3 } from "@components/providers";
+import { useRouter } from "next/router";
 
-import { useHooks } from "@components/providers/web3"
-import { useEffect } from "react"
-import { useWeb3 } from "@components/providers"
-import { useRouter } from "next/router"
-
-const _isEmpty = data => {
+const _isEmpty = (data) => {
   return (
     data == null ||
     data === "" ||
     (Array.isArray(data) && data.length === 0) ||
     (data.constructor === Object && Object.keys(data).length === 0)
-  )
-}
+  );
+};
 
-
-const enhanceHook = swrRes => {
-  const { data, error } = swrRes
-  const hasInitialResponse = !!(data || error)
-  const isEmpty = hasInitialResponse && _isEmpty(data)
+const enhanceHook = (swrRes) => {
+  const { data, error } = swrRes;
+  const hasInitialResponse = !!(data || error);
+  const isEmpty = hasInitialResponse && _isEmpty(data);
 
   return {
     ...swrRes,
     isEmpty,
-    hasInitialResponse
-  }
-}
+    hasInitialResponse,
+  };
+};
 
 export const useNetwork = () => {
-  const swrRes = enhanceHook(useHooks(hooks => hooks.useNetwork)())
+  const swrRes = enhanceHook(useHooks((hooks) => hooks.useNetwork)());
   return {
-    network: swrRes
-  }
-}
+    network: swrRes,
+  };
+};
 
 export const useAccount = () => {
-  const swrRes = enhanceHook(useHooks(hooks => hooks.useAccount)())
+  const swrRes = enhanceHook(useHooks((hooks) => hooks.useAccount)());
   return {
-    account: swrRes
-  }
-}
+    account: swrRes,
+  };
+};
 
-export const useAdmin = ({redirectTo}) => {
-  const { account } = useAccount()
-  const { requireInstall } = useWeb3()
-  const router = useRouter()
+export const useAdmin = ({ redirectTo }) => {
+  const { account } = useAccount();
+  const { requireInstall } = useWeb3();
+  const router = useRouter();
 
   useEffect(() => {
-    if ((
+    if (
       requireInstall ||
-      account.hasInitialResponse && !account.isAdmin) ||
-      account.isEmpty) {
-
-      router.push(redirectTo)
+      (account.hasInitialResponse && !account.isAdmin) ||
+      account.isEmpty
+    ) {
+      router.push(redirectTo);
     }
-  }, [account])
+  }, [account]);
 
-  return { account }
-}
+  return { account };
+};
 
 export const useOwnedCourses = (...args) => {
-  const swrRes = enhanceHook(useHooks(hooks => hooks.useOwnedCourses)(...args))
+  const swrRes = enhanceHook(
+    useHooks((hooks) => hooks.useOwnedCourses)(...args)
+  );
 
   return {
-    ownedCourses: swrRes
-  }
-}
+    ownedCourses: swrRes,
+  };
+};
 
 export const useOwnedCourse = (...args) => {
-  const swrRes = enhanceHook(useHooks(hooks => hooks.useOwnedCourse)(...args))
+  const swrRes = enhanceHook(
+    useHooks((hooks) => hooks.useOwnedCourse)(...args)
+  );
 
   return {
-    ownedCourse: swrRes
-  }
-}
+    ownedCourse: swrRes,
+  };
+};
 
 export const useManagedCourses = (...args) => {
-  const swrRes = enhanceHook(useHooks(hooks => hooks.useManagedCourses)(...args))
+  const swrRes = enhanceHook(
+    useHooks((hooks) => hooks.useManagedCourses)(...args)
+  );
 
   return {
-    managedCourses: swrRes
-  }
-}
+    managedCourses: swrRes,
+  };
+};
 
 export const useWalletInfo = () => {
-  const { account } = useAccount()
-  const { network } = useNetwork()
+  const { account } = useAccount();
+  const { network } = useNetwork();
+
+  const isConnecting =
+    !account.hasInitialResponse && !network.hasInitialResponse;
 
   return {
     account,
     network,
-    canPurchaseCourse: !!(account.data && network.isSupported)
-  }
-}
+    canPurchaseCourse: !!(account.data && network.isSupported),
+    isConnecting,
+    hasConnectedWallet: !!(account.data && network.isSupported),
+  };
+};
--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -1,87 +1,99 @@
+import { CourseCard, CourseList } from "@components/ui/course";
+import { BaseLayout } from "@components/ui/layout";
+import { getAllCourses } from "@content/courses/fetcher";
+import { useWalletInfo } from "@components/hooks/web3";
+import { Button, Loader } from "@components/ui/common";
+import { OrderModal } from "@components/ui/order";
+import { useState } from "react";
+import { MarketHeader } from "@components/ui/marketplace";
+import { useWeb3 } from "@components/providers";
 
+export default function Marketplace({ courses }) {
+  const { web3, contract, requireInstall } = useWeb3();
+  const { hasConnectedWallet, isConnecting, account } = useWalletInfo();
+  const [selectedCourse, setSelectedCourse] = useState(null);
 
-import { CourseCard, CourseList } from "@components/ui/course"
-import { BaseLayout } from "@components/ui/layout"
-import { getAllCourses } from "@content/courses/fetcher"
-import { useWalletInfo } from "@components/hooks/web3"
-import { Button } from "@components/ui/common"
-import { OrderModal } from "@components/ui/order"
-import { useState } from "react"
-import { MarketHeader } from "@components/ui/marketplace"
-import { useWeb3 } from "@components/providers"
-
-export default function Marketplace({courses}) {
-  const { web3, contract } = useWeb3()
-  const { canPurchaseCourse, account } = useWalletInfo()
-  const [selectedCourse, setSelectedCourse] = useState(null)
-
-  const purchaseCourse = async order => {
-    const hexCourseId = web3.utils.utf8ToHex(selectedCourse.id)
+  const purchaseCourse = async (order) => {
+    const hexCourseId = web3.utils.utf8ToHex(selectedCourse.id);
     const orderHash = web3.utils.soliditySha3(
       { type: "bytes16", value: hexCourseId },
       { type: "address", value: account.data }
-    )
-    const emailHash = web3.utils.sha3(order.email)
+    );
+    const emailHash = web3.utils.sha3(order.email);
     const proof = web3.utils.soliditySha3(
       { type: "bytes32", value: emailHash },
       { type: "bytes32", value: orderHash }
-    )
+    );
 
-    const value = web3.utils.toWei(String(order.price))
+    const value = web3.utils.toWei(String(order.price));
 
     try {
-      const result = await contract.methods.purchaseCourse(
-        hexCourseId,
-        proof
-      ).send({from: account.data, value})
-      console.log(result)
+      const result = await contract.methods
+        .purchaseCourse(hexCourseId, proof)
+        .send({ from: account.data, value });
+      console.log(result);
     } catch {
-      console.error("Purchase course: Operation has failed.")
+      console.error("Purchase course: Operation has failed.");
     }
-  }
+  };
 
   return (
     <>
       <MarketHeader />
-      <CourseList
-        courses={courses}
-      >
-      {course =>
-        <CourseCard
-          key={course.id}
-          course={course}
-          disabled={!canPurchaseCourse}
-          Footer={() =>
-            <div className="mt-4">
-              <Button
-                onClick={() => setSelectedCourse(course)}
-                disabled={!canPurchaseCourse}
-                variant="lightPurple">
-                Purchase
-              </Button>
-            </div>
-          }
-        />
-      }
+      <CourseList courses={courses}>
+        {(course) => (
+          <CourseCard
+            key={course.id}
+            course={course}
+            disabled={!hasConnectedWallet}
+            Footer={() => {
+              if (requireInstall) {
+                return (
+                  <Button disabled={true} variant="lightPurple">
+                    Install
+                  </Button>
+                );
+              }
+
+              if (isConnecting) {
+                return (
+                  <Button disabled={true} variant="lightPurple">
+                    <Loader size="sm" />
+                  </Button>
+                );
+              }
+
+              return (
+                <Button
+                  onClick={() => setSelectedCourse(course)}
+                  disabled={!hasConnectedWallet}
+                  variant="lightPurple"
+                >
+                  Purchase
+                </Button>
+              );
+            }}
+          />
+        )}
       </CourseList>
-      { selectedCourse &&
+      {selectedCourse && (
         <OrderModal
           course={selectedCourse}
           onSubmit={purchaseCourse}
           onClose={() => setSelectedCourse(null)}
         />
-      }
+      )}
     </>
-  )
+  );
 }
 
 export function getStaticProps() {
-  const { data } = getAllCourses()
+  const { data } = getAllCourses();
   return {
     props: {
-      courses: data
-    }
-  }
+      courses: data,
+    },
+  };
 }
 
-Marketplace.Layout = BaseLayout
+Marketplace.Layout = BaseLayout;
