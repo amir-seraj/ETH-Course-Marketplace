c04f9e7a185caf1e537726e9087483cc53ebba10
amirseraj <amirseraj.ir@gmail.com>
2022-11-23 11:30:06 +0330

    Filters Fixed

--- components/ui/order/modal/index.js
+++ components/ui/order/modal/index.js
@@ -1,5 +1,3 @@
-
-
 import { useEthPrice } from "@components/hooks/useEthPrice";
 import { Modal, Button } from "@components/ui/common";
 import { useEffect, useState } from "react";
@@ -7,177 +5,215 @@ import { useEffect, useState } from "react";
 const defaultOrder = {
   price: "",
   email: "",
-  confirmationEmail: ""
-}
+  confirmationEmail: "",
+};
 
-const _createFormState = (isDisabled = false, message =  "") => ({isDisabled, message})
+const _createFormState = (isDisabled = false, message = "") => ({
+  isDisabled,
+  message,
+});
 
-const createFormState = ({price, email, confirmationEmail}, hasAgreedTOS) => {
+const createFormState = (
+  { price, email, confirmationEmail },
+  hasAgreedTOS,
+  isNewPurchase
+) => {
   if (!price || Number(price) <= 0) {
-    return _createFormState(true, "Price is not valid.")
-  }
-  else if (confirmationEmail.length === 0 || email.length === 0) {
-    return _createFormState(true)
-  }
-  else if (email !== confirmationEmail) {
-    return _createFormState(true, "Email are not matching.")
-  }
-  else if (!hasAgreedTOS) {
-    return _createFormState(true, "You need to agree with terms of service in order to submit the form")
+    return _createFormState(true, "Price is not valid.");
   }
 
-  return _createFormState()
-}
+  if (isNewPurchase) {
+    if (confirmationEmail.length === 0 || email.length === 0) {
+      return _createFormState(true);
+    } else if (email !== confirmationEmail) {
+      return _createFormState(true, "Email are not matching.");
+    }
+  }
 
+  if (!hasAgreedTOS) {
+    return _createFormState(
+      true,
+      "You need to agree with terms of service in order to submit the form"
+    );
+  }
 
-export default function OrderModal({course, onClose, onSubmit}) {
-  const [isOpen, setIsOpen] = useState(false)
-  const [order, setOrder] = useState(defaultOrder)
-  const [enablePrice, setEnablePrice] = useState(false)
-  const [hasAgreedTOS, setHasAgreedTOS] = useState(false)
-  const { eth } = useEthPrice()
+  return _createFormState();
+};
+
+export default function OrderModal({
+  course,
+  onClose,
+  onSubmit,
+  isNewPurchase,
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [order, setOrder] = useState(defaultOrder);
+  const [enablePrice, setEnablePrice] = useState(false);
+  const [hasAgreedTOS, setHasAgreedTOS] = useState(false);
+  const { eth } = useEthPrice();
 
   useEffect(() => {
     if (!!course) {
-      setIsOpen(true)
+      setIsOpen(true);
       setOrder({
         ...defaultOrder,
-        price: eth.perItem
-      })
+        price: eth.perItem,
+      });
     }
-  }, [course])
+  }, [course]);
 
   const closeModal = () => {
-    setIsOpen(false)
-    setOrder(defaultOrder)
-    setEnablePrice(false)
-    setHasAgreedTOS(false)
-    onClose()
-  }
+    setIsOpen(false);
+    setOrder(defaultOrder);
+    setEnablePrice(false);
+    setHasAgreedTOS(false);
+    onClose();
+  };
 
-  const formState = createFormState(order, hasAgreedTOS)
+  const formState = createFormState(order, hasAgreedTOS, isNewPurchase);
 
   return (
     <Modal isOpen={isOpen}>
-      <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-        <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+      <div className="inline-block overflow-hidden text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+        <div className="px-4 pt-5 pb-4 bg-white sm:p-6 sm:pb-4">
           <div className="sm:flex sm:items-start">
             <div className="mt-3 sm:mt-0 sm:ml-4 sm:text-left">
-              <h3 className="mb-7 text-lg font-bold leading-6 text-gray-900" id="modal-title">
+              <h3
+                className="text-lg font-bold leading-6 text-gray-900 mb-7"
+                id="modal-title"
+              >
                 {course.title}
               </h3>
-              <div className="mt-1 relative rounded-md">
+              <div className="relative mt-1 rounded-md">
                 <div className="mb-1">
                   <label className="mb-2 font-bold">Price(eth)</label>
-                  <div className="text-xs text-gray-700 flex">
+                  <div className="flex text-xs text-gray-700">
                     <label className="flex items-center mr-2">
                       <input
                         checked={enablePrice}
-                        onChange={({target: {checked}}) => {
+                        onChange={({ target: { checked } }) => {
                           setOrder({
                             ...order,
-                            price: checked ? order.price : eth.perItem
-                          })
-                          setEnablePrice(checked)
+                            price: checked ? order.price : eth.perItem,
+                          });
+                          setEnablePrice(checked);
                         }}
                         type="checkbox"
                         className="form-checkbox"
                       />
                     </label>
-                    <span>Adjust Price - only when the price is not correct</span>
+                    <span>
+                      Adjust Price - only when the price is not correct
+                    </span>
                   </div>
                 </div>
                 <input
                   disabled={!enablePrice}
                   value={order.price}
-                  onChange={({target: {value}}) => {
-                    if (isNaN(value)) { return; }
+                  onChange={({ target: { value } }) => {
+                    if (isNaN(value)) {
+                      return;
+                    }
                     setOrder({
                       ...order,
-                      price: value
-                    })
+                      price: value,
+                    });
                   }}
                   type="text"
                   name="price"
                   id="price"
-                  className="disabled:opacity-50 w-80 mb-1 focus:ring-indigo-500 shadow-md focus:border-indigo-500 block pl-7 p-4 sm:text-sm border-gray-300 rounded-md"
+                  className="block p-4 mb-1 border-gray-300 rounded-md shadow-md disabled:opacity-50 w-80 focus:ring-indigo-500 focus:border-indigo-500 pl-7 sm:text-sm"
                 />
                 <p className="text-xs text-gray-700">
-                  Price will be verified at the time of the order. If the price will be lower, order can be declined (+- 2% slipage is allowed)
+                  Price will be verified at the time of the order. If the price
+                  will be lower, order can be declined (+- 2% slipage is
+                  allowed)
                 </p>
               </div>
-              <div className="mt-2 relative rounded-md">
-                <div className="mb-1">
-                  <label className="mb-2 font-bold">Email</label>
-                </div>
-                <input
-                  onChange={({target: {value}}) => {
-                    setOrder({
-                      ...order,
-                      email: value.trim()
-                    })
-                  }}
-                  type="email"
-                  name="email"
-                  id="email"
-                  className="w-80 focus:ring-indigo-500 shadow-md focus:border-indigo-500 block pl-7 p-4 sm:text-sm border-gray-300 rounded-md"
-                  placeholder="x@y.com"
-                />
-                <p className="text-xs text-gray-700 mt-1">
-                It&apos;s important to fill a correct email, otherwise the order cannot be verified. We are not storing your email anywhere
-                </p>
-              </div>
-              <div className="my-2 relative rounded-md">
-                <div className="mb-1">
-                  <label className="mb-2 font-bold">Repeat Email</label>
-                </div>
-                <input
-                  onChange={({target: {value}}) => {
-                    setOrder({
-                      ...order,
-                      confirmationEmail: value.trim()
-                    })
-                  }}
-                  type="email"
-                  name="confirmationEmail"
-                  id="confirmationEmail"
-                  className="w-80 focus:ring-indigo-500 shadow-md focus:border-indigo-500 block pl-7 p-4 sm:text-sm border-gray-300 rounded-md" placeholder="x@y.com" />
-              </div>
-              <div className="text-xs text-gray-700 flex">
+              {isNewPurchase && (
+                <>
+                  <div className="relative mt-2 rounded-md">
+                    <div className="mb-1">
+                      <label className="mb-2 font-bold">Email</label>
+                    </div>
+                    <input
+                      onChange={({ target: { value } }) => {
+                        setOrder({
+                          ...order,
+                          email: value.trim(),
+                        });
+                      }}
+                      type="email"
+                      name="email"
+                      id="email"
+                      className="block p-4 border-gray-300 rounded-md shadow-md w-80 focus:ring-indigo-500 focus:border-indigo-500 pl-7 sm:text-sm"
+                      placeholder="x@y.com"
+                    />
+                    <p className="mt-1 text-xs text-gray-700">
+                      It&apos;s important to fill a correct email, otherwise the
+                      order cannot be verified. We are not storing your email
+                      anywhere
+                    </p>
+                  </div>
+                  <div className="relative my-2 rounded-md">
+                    <div className="mb-1">
+                      <label className="mb-2 font-bold">Repeat Email</label>
+                    </div>
+                    <input
+                      onChange={({ target: { value } }) => {
+                        setOrder({
+                          ...order,
+                          confirmationEmail: value.trim(),
+                        });
+                      }}
+                      type="email"
+                      name="confirmationEmail"
+                      id="confirmationEmail"
+                      className="block p-4 border-gray-300 rounded-md shadow-md w-80 focus:ring-indigo-500 focus:border-indigo-500 pl-7 sm:text-sm"
+                      placeholder="x@y.com"
+                    />
+                  </div>
+                </>
+              )}
+              <div className="flex mt-5 text-xs text-gray-700">
                 <label className="flex items-center mr-2">
                   <input
                     checked={hasAgreedTOS}
-                    onChange={({target: {checked}}) => {
-                      setHasAgreedTOS(checked)
+                    onChange={({ target: { checked } }) => {
+                      setHasAgreedTOS(checked);
                     }}
                     type="checkbox"
-                    className="form-checkbox" />
+                    className="form-checkbox"
+                  />
                 </label>
-                <span>I accept Eincode &apos;terms of service&apos; and I agree that my order can be rejected in the case data provided above are not correct</span>
+                <span>
+                  I accept Eincode &apos;terms of service&apos; and I agree that
+                  my order can be rejected in the case data provided above are
+                  not correct
+                </span>
               </div>
-              { formState.message &&
-                <div className="p-4 my-3 text-yellow-700 bg-yellow-200 rounded-lg text-sm">
-                  { formState.message }
+              {formState.message && (
+                <div className="p-4 my-3 text-sm text-yellow-700 bg-yellow-200 rounded-lg">
+                  {formState.message}
                 </div>
-              }
+              )}
             </div>
           </div>
         </div>
-        <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex">
+        <div className="px-4 py-3 bg-gray-50 sm:px-6 sm:flex">
           <Button
             disabled={formState.isDisabled}
             onClick={() => {
-              onSubmit(order)
-          }}>
+              onSubmit(order);
+            }}
+          >
             Submit
           </Button>
-          <Button
-            onClick={closeModal}
-            variant="red">
+          <Button onClick={closeModal} variant="red">
             Cancel
           </Button>
         </div>
       </div>
     </Modal>
-  )
+  );
 }
--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -1,7 +1,7 @@
 import { CourseCard, CourseList } from "@components/ui/course";
 import { BaseLayout } from "@components/ui/layout";
 import { getAllCourses } from "@content/courses/fetcher";
-import { useWalletInfo, useOwnedCourses } from "@components/hooks/web3";
+import { useOwnedCourses, useWalletInfo } from "@components/hooks/web3";
 import { Button, Loader, Message } from "@components/ui/common";
 import { OrderModal } from "@components/ui/order";
 import { useState } from "react";
@@ -11,8 +11,10 @@ import { useWeb3 } from "@components/providers";
 export default function Marketplace({ courses }) {
   const { web3, contract, requireInstall } = useWeb3();
   const { hasConnectedWallet, isConnecting, account } = useWalletInfo();
-  const [selectedCourse, setSelectedCourse] = useState(null);
   const { ownedCourses } = useOwnedCourses(courses, account.data);
+
+  const [selectedCourse, setSelectedCourse] = useState(null);
+  const [isNewPurchase, setIsNewPurchase] = useState(true);
 
   const purchaseCourse = async (order) => {
     const hexCourseId = web3.utils.utf8ToHex(selectedCourse.id);
@@ -20,17 +22,37 @@ export default function Marketplace({ courses }) {
       { type: "bytes16", value: hexCourseId },
       { type: "address", value: account.data }
     );
-    const emailHash = web3.utils.sha3(order.email);
-    const proof = web3.utils.soliditySha3(
-      { type: "bytes32", value: emailHash },
-      { type: "bytes32", value: orderHash }
-    );
 
     const value = web3.utils.toWei(String(order.price));
 
+    if (isNewPurchase) {
+      const emailHash = web3.utils.sha3(order.email);
+      const proof = web3.utils.soliditySha3(
+        { type: "bytes32", value: emailHash },
+        { type: "bytes32", value: orderHash }
+      );
+
+      _purchaseCourse(hexCourseId, proof, value);
+    } else {
+      _repurchaseCourse(orderHash, value);
+    }
+  };
+
+  const _purchaseCourse = async (hexCourseId, proof, value) => {
     try {
       const result = await contract.methods
         .purchaseCourse(hexCourseId, proof)
+        .send({ from: account.data, value });
+      console.log(result);
+    } catch {
+      console.error("Purchase course: Operation has failed.");
+    }
+  };
+
+  const _repurchaseCourse = async (courseHash, value) => {
+    try {
+      const result = await contract.methods
+        .repurchaseCourse(courseHash)
         .send({ from: account.data, value });
       console.log(result);
     } catch {
@@ -42,74 +64,91 @@ export default function Marketplace({ courses }) {
     <>
       <MarketHeader />
       <CourseList courses={courses}>
-        {(course) => (
-          <CourseCard
-            key={course.id}
-            course={course}
-            disabled={!hasConnectedWallet}
-            Footer={() => {
-              if (requireInstall) {
-                return (
-                  <Button disabled={true} variant="lightPurple" size="sm">
-                    Install
-                  </Button>
-                );
-              }
+        {(course) => {
+          const owned = ownedCourses.lookup[course.id];
+          return (
+            <CourseCard
+              key={course.id}
+              course={course}
+              state={owned?.state}
+              disabled={!hasConnectedWallet}
+              Footer={() => {
+                if (requireInstall) {
+                  return (
+                    <Button size="sm" disabled={true} variant="lightPurple">
+                      Install
+                    </Button>
+                  );
+                }
 
-              if (isConnecting) {
-                return (
-                  <Button disabled={true} variant="lightPurple">
-                    <Loader size="sm" />
-                  </Button>
-                );
-              }
-              if (!ownedCourses.hasInitialResponse) {
-                return <div style={{ height: "50px" }}></div>;
-              }
+                if (isConnecting) {
+                  return (
+                    <Button size="sm" disabled={true} variant="lightPurple">
+                      <Loader size="sm" />
+                    </Button>
+                  );
+                }
 
-              const owned = ownedCourses.lookup[course.id];
+                if (!ownedCourses.hasInitialResponse) {
+                  return <div style={{ height: "42px" }}></div>;
+                }
 
-              if (owned) {
-                return (
-                  <>
-                    <div>
-                      <Button disabled={true} variant="green" size="sm">
-                        Owned
-                      </Button>
-                      {owned.state === "deactivated" && (
+                if (owned) {
+                  return (
+                    <>
+                      <div className="flex">
                         <Button
+                          onClick={() => alert("You are owner of this course.")}
                           disabled={false}
-                          onClick={() => alert("Re-activating")}
-                          variant="purple"
                           size="sm"
+                          variant="white"
                         >
-                          Fund to Activate
+                          Yours &#10004;
                         </Button>
-                      )}
-                    </div>
-                  </>
-                );
-              }
+                        {owned.state === "deactivated" && (
+                          <div className="ml-1">
+                            <Button
+                              size="sm"
+                              disabled={false}
+                              onClick={() => {
+                                setIsNewPurchase(false);
+                                setSelectedCourse(course);
+                              }}
+                              variant="purple"
+                            >
+                              Fund to Activate
+                            </Button>
+                          </div>
+                        )}
+                      </div>
+                    </>
+                  );
+                }
 
-              return (
-                <Button
-                  onClick={() => setSelectedCourse(course)}
-                  disabled={!hasConnectedWallet}
-                  variant="lightPurple"
-                  size="sm"
-                >
-                  Purchase
-                </Button>
-              );
-            }}
-          />
-        )}
+                return (
+                  <Button
+                    onClick={() => setSelectedCourse(course)}
+                    size="sm"
+                    disabled={hasConnectedWallet}
+                    variant="lightPurple"
+                  >
+                    Purchase
+                  </Button>
+                );
+              }}
+            />
+          );
+        }}
       </CourseList>
       {selectedCourse && (
         <OrderModal
           course={selectedCourse}
+          isNewPurchase={isNewPurchase}
           onSubmit={purchaseCourse}
-          onClose={() => setSelectedCourse(null)}
+          onClose={() => {
+            setSelectedCourse(null);
+            setIsNewPurchase(true);
+          }}
         />
       )}
     </>
