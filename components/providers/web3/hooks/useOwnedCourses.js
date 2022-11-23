fc53b907f7b703b1d063e600777dd9abedcb5525
amirseraj <amirseraj.ir@gmail.com>
2022-11-23 07:36:22 +0330

    lookup table

--- components/providers/web3/hooks/useOwnedCourses.js
+++ components/providers/web3/hooks/useOwnedCourses.js
@@ -1,33 +1,43 @@
-
-
-import { createCourseHash } from "@utils/hash"
-import { normalizeOwnedCourse } from "@utils/normalize"
-import useSWR from "swr"
+import { createCourseHash } from "@utils/hash";
+import { normalizeOwnedCourse } from "@utils/normalize";
+import useSWR from "swr";
 
 export const handler = (web3, contract) => (courses, account) => {
-
-  const swrRes = useSWR(() =>
-    (web3 && contract && account) ? `web3/ownedCourses/${account}` : null,
+  const swrRes = useSWR(
+    () => (web3 && contract && account ? `web3/ownedCourses/${account}` : null),
     async () => {
-      const ownedCourses = []
+      const ownedCourses = [];
 
       for (let i = 0; i < courses.length; i++) {
-        const course = courses[i]
+        const course = courses[i];
 
-        if (!course.id) { continue }
+        if (!course.id) {
+          continue;
+        }
 
-        const courseHash = createCourseHash(web3)(course.id, account)
-        const ownedCourse = await contract.methods.getCourseByHash(courseHash).call()
+        const courseHash = createCourseHash(web3)(course.id, account);
+        const ownedCourse = await contract.methods
+          .getCourseByHash(courseHash)
+          .call();
 
-        if (ownedCourse.owner !== "0x0000000000000000000000000000000000000000") {
-          const normalized = normalizeOwnedCourse(web3)(course, ownedCourse)
-          ownedCourses.push(normalized)
+        if (
+          ownedCourse.owner !== "0x0000000000000000000000000000000000000000"
+        ) {
+          const normalized = normalizeOwnedCourse(web3)(course, ownedCourse);
+          ownedCourses.push(normalized);
         }
       }
 
-      return ownedCourses
+      return ownedCourses;
     }
-  )
+  );
 
-  return swrRes
-}
+  return {
+    ...swrRes,
+    lookup:
+      swrRes.data?.reduce((a, c) => {
+        a[c.id] = c;
+        return a;
+      }, {}) ?? {},
+  };
+};
--- components/ui/course/card/index.js
+++ components/ui/course/card/index.js
@@ -1,12 +1,9 @@
+import Image from "next/image";
+import Link from "next/link";
 
-
-import Image from "next/image"
-import Link from "next/link"
-
-export default function Card({course, disabled, Footer}) {
+export default function Card({ course, disabled, Footer }) {
   return (
-    <div
-      className="bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+    <div className="overflow-hidden bg-white shadow-md rounded-xl md:max-w-2xl">
       <div className="flex h-full">
         <div className="flex-1 h-full next-image-wrapper">
           <Image
@@ -19,25 +16,24 @@ export default function Card({course, disabled, Footer}) {
           />
         </div>
         <div className="p-8 pb-4 flex-2">
-          <div
-            className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+          <div className="text-sm font-semibold tracking-wide text-indigo-500 uppercase">
             {course.type}
           </div>
           <Link href={`/courses/${course.slug}`}>
-            <a
-              className="h-12 block mt-1 text-sm sm:text-lg leading-tight font-medium text-black hover:underline">
+            <a className="block h-12 mt-1 text-sm font-medium leading-tight text-black sm:text-lg hover:underline">
               {course.title}
             </a>
           </Link>
-          <p
-            className="mt-2 text-sm sm:text-base text-gray-500">
+          <p className="mt-2 text-sm text-gray-500 sm:text-base">
             {course.description.substring(0, 70)}...
           </p>
-          { Footer &&
-            <Footer />
-          }
+          {Footer && (
+            <div className="mt-2">
+              <Footer />
+            </div>
+          )}
         </div>
       </div>
     </div>
-  )
+  );
 }
