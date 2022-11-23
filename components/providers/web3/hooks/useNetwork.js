ecb749496e0c0b5eea66b9568fb5825f2532b9e4
amirseraj <amirseraj.ir@gmail.com>
2022-11-23 07:47:55 +0330

    Listener setup

--- components/providers/web3/hooks/useNetwork.js
+++ components/providers/web3/hooks/useNetwork.js
@@ -13,8 +13,8 @@ const NETWORKS = {
 
 const targetNetwork = NETWORKS[process.env.NEXT_PUBLIC_TARGET_CHAIN_ID];
 
-export const handler = (web3, provider) => () => {
-  const { data, mutate, ...rest } = useSWR(
+export const handler = (web3) => () => {
+  const { data, ...rest } = useSWR(
     () => (web3 ? "web3/network" : null),
     async () => {
       const chainId = await web3.eth.getChainId();
@@ -27,18 +27,8 @@ export const handler = (web3, provider) => () => {
     }
   );
 
-  useEffect(() => {
-    const mutator = (chainId) => window.location.reload();
-    provider?.on("chainChanged", mutator);
-
-    return () => {
-      provider?.removeListener("chainChanged", mutator);
-    };
-  }, [provider]);
-
   return {
     data,
-    mutate,
     target: targetNetwork,
     isSupported: data === targetNetwork,
     ...rest,
