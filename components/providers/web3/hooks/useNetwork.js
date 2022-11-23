361cfbb2c671a934b901fa40e3ab8098b01808b2
amirseraj <amirseraj.ir@gmail.com>
2022-11-23 07:44:51 +0330

    handle network switching

--- components/providers/web3/hooks/useNetwork.js
+++ components/providers/web3/hooks/useNetwork.js
@@ -1,5 +1,5 @@
-import { useEffect } from "react"
-import useSWR from "swr"
+import { useEffect } from "react";
+import useSWR from "swr";
 
 const NETWORKS = {
   1: "Ethereum Main Network",
@@ -9,39 +9,38 @@ const NETWORKS = {
   42: "Kovan Test Network",
   56: "Binance Smart Chain",
   1337: "Ganache",
-}
+};
 
-const targetNetwork = NETWORKS[process.env.NEXT_PUBLIC_TARGET_CHAIN_ID]
+const targetNetwork = NETWORKS[process.env.NEXT_PUBLIC_TARGET_CHAIN_ID];
 
 export const handler = (web3, provider) => () => {
-
-  const { data, mutate, ...rest } = useSWR(() =>
-    web3 ? "web3/network" : null,
+  const { data, mutate, ...rest } = useSWR(
+    () => (web3 ? "web3/network" : null),
     async () => {
-      const chainId = await web3.eth.getChainId()
+      const chainId = await web3.eth.getChainId();
 
       if (!chainId) {
-        throw new Error("Cannot retreive network. Please refresh the browser.")
+        throw new Error("Cannot retreive network. Please refresh the browser.");
       }
 
-      return NETWORKS[chainId]
+      return NETWORKS[chainId];
     }
-  )
+  );
 
   useEffect(() => {
-    const mutator = chainId => mutate(NETWORKS[parseInt(chainId, 16)])
-    provider?.on("chainChanged", mutator)
+    const mutator = (chainId) => window.location.reload();
+    provider?.on("chainChanged", mutator);
 
     return () => {
-      provider?.removeListener("chainChanged", mutator)
-    }
-  }, [provider])
+      provider?.removeListener("chainChanged", mutator);
+    };
+  }, [provider]);
 
   return {
     data,
     mutate,
     target: targetNetwork,
     isSupported: data === targetNetwork,
-    ...rest
-  }
-}
+    ...rest,
+  };
+};
